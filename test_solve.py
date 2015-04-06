c3090a59dd45ebf192c841084dee77ce2724b201
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2015-04-06 16:46:06 -0700

    added skew-T tests

--- test_solve.py
+++ test_solve.py
@@ -199,5 +199,56 @@ class calculateTests(unittest.TestCase):
         assert isinstance(Tv, np.ndarray)
 
 
+class TestSolveValuesNearSkewT(unittest.TestCase):
+
+    def setUp(self):
+        self.quantities = {'p': 8.9e4, 'T': 9.+273.15, 'theta': 17.9+273.15,
+                           'rv': 6e-3, 'Tlcl': 4.+273.15, 'thetae': 36.+273.15,
+                           'Tw': 7.+273.15, 'Td': 4.8+273.15, 'plcl': 83500.,
+                           'thetaae': 36.+273.15, 'thetaie': 36.+273.15,
+                           }
+
+    def _generator(self, quantity, tolerance):
+        skew_T_value = self.quantities.pop(quantity)
+        calculated_value = calculate(quantity, methods=default_methods +
+                                     ('bolton',), **self.quantities)
+        assert abs(skew_T_value - calculated_value) < tolerance
+
+    def tearDown(self):
+        self.quantities = None
+
+    def test_calculate_p(self):
+        self._generator('p', 10000.)
+
+    def test_calculate_T(self):
+        self._generator('T', 1.)
+
+    def test_calculate_theta(self):
+        self._generator('theta', 1.)
+
+    def test_calculate_rv(self):
+        self._generator('rv', 1e-3)
+
+    def test_calculate_Tlcl(self):
+        self._generator('Tlcl', 1.)
+
+    def test_calculate_thetae(self):
+        self._generator('thetae', 1.)
+
+    def test_calculate_Tw(self):
+        self._generator('Tw', 1.)
+
+    def test_calculate_Td(self):
+        self._generator('Td', 1.)
+
+    def test_calculate_plcl(self):
+        self._generator('plcl', 10000.)
+
+    def test_calculate_thetaae(self):
+        self._generator('thetaae', 1.)
+
+    def test_calculate_thetaie(self):
+        self._generator('thetaie', 1.)
+
 if __name__ == '__main__':
     nose.run()
