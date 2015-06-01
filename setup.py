d0e224d57f170063b66610d85cf915746c372b6d
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2015-06-01 12:26:55 -0700

    fixed formatting in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Atmospheric sciences utility library',
     author='Jeremy McGibbon',
     author_email='mcgibbon@uw.edu',
-    url='https://github.com/mcgibbon/atmos'
+    url='https://github.com/mcgibbon/atmos',
     keywords=['atmos', 'atmospheric', 'equations', 'geoscience', 'science'],
     classifiers=[],
 )
