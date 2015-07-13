ceb8af765730d1dc1ae02fc9e0c7b566dee65ba5
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2015-07-13 11:19:33 -0700

    corrected requirements to actually be required when using pip

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='atmos',
@@ -7,6 +15,7 @@ setup(
     description='Atmospheric sciences utility library',
     author='Jeremy McGibbon',
     author_email='mcgibbon@uw.edu',
+    install_requires = reqs,
     url='https://github.com/mcgibbon/atmos',
     keywords=['atmos', 'atmospheric', 'equations', 'geoscience', 'science'],
     classifiers=[],
