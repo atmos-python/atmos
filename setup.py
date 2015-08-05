25b01328983b54222c4c7e5aef63c53885b6f075
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2015-08-04 19:05:43 -0700

    changed unittest2 requirement to be for python 2.6 only

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from pip.req import parse_requirements
+import sys
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements('requirements.txt', session=False)
@@ -7,6 +8,8 @@ install_reqs = parse_requirements('requirements.txt', session=False)
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 reqs = [str(ir.req) for ir in install_reqs]
+if sys.version_info[:2] == (2, 6):
+    reqs.append('unittest2>=0.5.1')
 
 setup(
     name='atmos',
