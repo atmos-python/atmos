fa31878d8ee4e0b5611c442a03585e1befc63e05
Bryan Woods <bryan.woods@here.com>
2018-07-16 21:05:06 -0400

    removing parse_requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 from setuptools import setup
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt', session=False)
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
+with open('requirements.txt') as f:
+    install_reqs = f.read().strip().split('\n')
 
 setup(
     name='atmos',
@@ -18,7 +10,7 @@ setup(
     description='Atmospheric sciences utility library',
     author='Jeremy McGibbon',
     author_email='mcgibbon@uw.edu',
-    install_requires=reqs,
+    install_requires=install_reqs,
     url='https://github.com/mcgibbon/atmos',
     keywords=['atmos', 'atmospheric', 'equations', 'geoscience', 'science'],
     classifiers=[],
